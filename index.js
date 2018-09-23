422bc53b07fde960c4356fae8d60673fcfdc08b0
Hermann <hermannsblum@yahoo.de>
2018-09-23 18:27:26 +0200

    add index,js to expose ui elements from npm package

---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as DatalistController } from './src/datalistcontroller';
+export { default as DropdownCard } from './src/dropdowncard';
+export { default as Form } from './src/form';
+export { textInput, numInput, datetimeInput, fileInput } from './src/inputFields';
+export { default as ListSelect } from './src/listselect';
