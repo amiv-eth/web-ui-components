45cb1447e04b1ac5fd025ee1e92f41785a5224c2
Hermann <hermannsblum@yahoo.de>
2018-09-24 18:57:11 +0200

    fix critical: getDate was chifting month at every edit

--- src/inputFields.js
+++ src/inputFields.js
@@ -144,7 +144,7 @@ export class datetimeInput {
         year: parsed.getFullYear(),
         month: `${parsed.getMonth()}`.padStart(2, '0'),
         date: `${parsed.getDate()}`.padStart(2, '0'),
-        hour: `${parsed.getHours()}`.padStart(2, '0'),
+        hour: `${parsed.getHours() + 1}`.padStart(2, '0'),
         minute: `${parsed.getMinutes()}`.padStart(2, '0'),
       };
       initialDate = `${locale.year}-${locale.month}-${locale.date}`;
