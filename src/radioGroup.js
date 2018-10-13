aae590f866fa64ac5f24410861762838e1f4f148
sgoff <sgoff@student.ethz.ch>
2018-10-13 16:17:04 +0200

    Add RadioGroup component

---
+++ src/radioGroup.js
@@ -0,0 +1,44 @@
+import m from 'mithril';
+import { RadioButton } from 'polythene-mithril';
+
+/**
+ * Generic RadioGroup component
+ *
+ * Attributes:
+ *
+ *   - `value` current checked value
+ *   - `default` default checked value
+ *   - `values` array of possible values like [{label: '', value: ''}]
+ *   - `onchange` called whenever value changes with value as argument
+ *
+ * @return {RadioGroupComponent} generic checkbox as mithril component.
+ */
+export default class RadioGroup {
+  constructor(vnode) {
+    this.value = vnode.attrs.value || vnode.attrs.default;
+  }
+  onupdate(vnode) {
+    if (vnode.attrs.value && vnode.attrs.value !== this.value) {
+      this.value = vnode.attrs.value;
+    }
+  }
+  view(vnode) {
+    const buttons = [];
+    vnode.attrs.values.forEach((option, index) => {
+      if (index) {
+        buttons.push(m('br'));
+      }
+      buttons.push(m(RadioButton, {
+        ...option,
+        checked: this.value === option.value,
+        onChange: (state) => {
+          if (state.checked) {
+            this.value = option.value;
+            vnode.attrs.onchange(this.value);
+          }
+        },
+      }));
+    });
+    return m('div', buttons);
+  }
+}
