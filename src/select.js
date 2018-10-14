c3d2d9922cd45b45691966ee701c619dc006d148
Sandro Lutz <code@temparus.ch>
2018-10-14 03:19:44 +0200

    Add documentation

--- src/select.js
+++ src/select.js
@@ -3,6 +3,18 @@ import '@material/select/dist/mdc.select.css';
 import '@material/select/dist/mdc.select';
 
 export default class Select {
+  /*
+   * A selection field where the value can be choosen from a small given set.
+   *
+   * Attrs:
+   *   name: string
+   *     name of the form field.
+   *   label: string
+   *     Label which is shown above the selected value.
+   *   onchange: funtion()
+   *     Called when the selection has changed.
+   */
+
   static view({ attrs: { options, name, label = '', onchange = () => {}, ...kwargs } }) {
     return m('div.mdc-select', { style: { height: '41px' } }, [
       m('select.mdc-select__native-control', {
