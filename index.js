b1fb1eba5b149a7f083c70b949437a067fee8e5d
Hermann <hermannsblum@yahoo.de>
2018-10-15 20:44:22 +0200

    fix export of correct namings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export { default as DatalistController } from './src/datalistController';
 export { default as DropdownCard } from './src/dropdownCard';
 export { default as Form } from './src/form';
-export { textInput, numInput, datetimeInput, fileInput } from './src/inputFields';
+export { TextInput, NumInput, DatetimeInput, FileInput } from './src/inputFields';
 export { default as ListSelect } from './src/listSelect';
 export { default as RadioGroup } from './src/radioGroup';
