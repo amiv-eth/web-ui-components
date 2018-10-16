77f53357ba6811124d52ef9bf83893c759ab3065
Sandro Lutz <code@temparus.ch>
2018-10-16 10:05:52 +0200

    Change to JSDoc documentation

--- src/select.js
+++ src/select.js
@@ -3,18 +3,17 @@ import '@material/select/dist/mdc.select.css';
 import '@material/select/dist/mdc.select';
 
 export default class Select {
-  /*
+  /**
    * A selection field where the value can be choosen from a small given set.
    *
-   * Attrs:
-   *   name: string
-   *     name of the form field.
-   *   label: string
-   *     Label which is shown above the selected value.
-   *   onchange: funtion()
-   *     Called when the selection has changed.
+   * @param {object}   attrs          unless specified below, attrs will be
+   *   passed into the <select/> input field.
+   * @param {string}   attrs.name     name of the form field.
+   * @param {string}   attrs.label    Text which is shown above the selected value.
+   * @param {function} attrs.onchange function (name, value) -
+   *   this function is called every time that the value of the select changes
+   *   (i.e. not the focus, as would happen with polythene inputs)
    */
-
   static view({ attrs: { options, name, label = '', onchange = () => {}, ...kwargs } }) {
     return m('div.mdc-select', { style: { height: '41px' } }, [
       m('select.mdc-select__native-control', {
