a7dedf57e131d8b9e2d702b95e6fd34dc29d9a81
scmoritz <moritz.schneider@inf.ethz.ch>
2018-12-02 23:05:43 +0100

    Add material styling to select

--- src/select.js
+++ src/select.js
@@ -1,6 +1,7 @@
 import m from 'mithril';
 import '@material/select/dist/mdc.select.css';
 import '@material/select/dist/mdc.select';
+import { MDCSelect } from '@material/select';
 
 export default class Select {
   /**
@@ -10,19 +11,74 @@ export default class Select {
    *   passed into the <select/> input field.
    * @param {string}   attrs.name     name of the form field.
    * @param {string}   attrs.label    Text which is shown above the selected value.
-   * @param {function} attrs.onchange function (name, value) -
+   * @param {function} attrs.onChange function (name, value) -
    *   this function is called every time that the value of the select changes
    *   (i.e. not the focus, as would happen with polythene inputs)
+   * @param {array}    attrs.options  Array containing the selectable items
+   * @param {string}   attrs.value    Selected value
    */
-  static view({ attrs: { options, name, label = '', onchange = () => {}, ...kwargs } }) {
-    return m('div.mdc-select', { style: { height: '41px' } }, [
-      m('select.mdc-select__native-control', {
-        style: { 'padding-top': '10px' },
-        onchange: ({ target: { value } }) => { onchange(value); },
-        ...kwargs,
-      }, options.map(option => m('option', { value: option }, option))),
-      m('label', { class: 'mdc-floating-label mdc-floating-label--float-above' }, label),
-      m('div.mdc-line-ripple'),
+
+  oncreate({ dom }) {
+    this.foo = new MDCSelect(dom.querySelector(`#${this.name}`));
+  }
+
+  view({
+    attrs: { options, name, label = '', onChange = () => {}, getErrors = () => [], ...kwargs },
+  }) {
+    this.name = name;
+    let rippleColor = { 'background-color': 'rgba(0, 0, 0, 0.6)' };
+    let textColor = { color: 'rgba(0, 0, 0, 0.6)' };
+    let borderBottomColor = { 'border-bottom-color': 'rgba(0, 0, 0, 0.6)' };
+
+    if (getErrors().length !== 0) {
+      rippleColor = { 'background-color': 'rgba(221, 44, 0, 1)' };
+      textColor = { color: 'rgba(221, 44, 0, 1)' };
+      borderBottomColor = { 'border-bottom-color': 'rgba(221, 44, 0, 1)' };
+    }
+    return m('div', [
+      m(
+        'div.mdc-select',
+        {
+          id: name,
+          'aria-controls': `${name}-helper-text`,
+          'aria-describedby': `${name}-helper-text`,
+        },
+        [
+          m(
+            'select.mdc-select__native-control',
+            {
+              style: borderBottomColor,
+              onchange: ({ target: { value } }) => {
+                onChange(name, value);
+              },
+              ...kwargs,
+            },
+            [
+              m.trust('<option value="" disabled selected></option>'),
+              ...options.map(option => m('option', { value: option }, option)),
+            ],
+          ),
+          m(
+            'label',
+            {
+              class: 'mdc-floating-label mdc-floating-label--float-above',
+              style: textColor,
+            },
+            label,
+          ),
+          m('div.mdc-line-ripple', {
+            style: rippleColor,
+          }),
+        ],
+      ),
+      m(
+        'p.pe-textfield-error',
+        {
+          id: `${name}-helper-text`,
+          style: { color: 'rgba(221, 44, 0, 1)', 'font-size': '12px' },
+        },
+        getErrors(),
+      ),
     ]);
   }
 }
