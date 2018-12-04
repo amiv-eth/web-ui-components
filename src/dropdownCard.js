98ce6951067c93cae5ffc811a580369592f5b9eb
Sandro Lutz <code@temparus.ch>
2018-12-04 23:05:28 +0100

    Update DropdownCard component

--- src/dropdownCard.js
+++ src/dropdownCard.js
@@ -1,47 +1,43 @@
 import m from 'mithril';
-import { Toolbar, IconButton, ToolbarTitle, Card } from 'polythene-mithril';
-import icons from './icons';
+import { Toolbar, ToolbarTitle } from 'polythene-mithril';
+import ExpansionPanel from './expansionPanel';
 
-/** A [card](https://arthurclemens.github.io/polythene-demos/mithril/#/card) that is usually
- * collapsed, but pops out when clicked on the title */
 export default class DropdownCard {
   /**
-   * @param {objects} attrs         any attributes for a Card from polythene
-   * @param {string}  attrs.title   the title of the card -
+   * A [card](https://arthurclemens.github.io/polythene-demos/mithril/#/card) that is usually
+   * collapsed, but pops out when clicked on the title
+   *
+   * @param {objects}   attrs           Any attributes for a Card from polythene
+   * @param {string}    attrs.title     The title of the card -
    *   this will be the only thing visible if the card is collapsed
-   * @param children          any content in the polythene card
+   * @param {boolean}   attrs.expanded  Flag indicating if the component should be
+   *   expanded. After a change of this attribute, the animation is performed.
+   * @param {boolean}   attrs.separated Flag indicating if margin should be added at
+   *   the top/bottom of the component when expanded.
+   * @param {string}    attrs.duration  Duration of the enter/exit animations
+   *   (default: 300ms)
+   * @param {string}    attrs.animation CSS animation
+   *   (default: cubic-bezier(0.4, 0, 0.2, 1)
+   * @param {function}  attrs.onChange  Callback when state (expanded or not) has changed
+   * @param {Component} children        Any content in the polythene card
    */
-  constructor() {
-    this.expand = false;
-  }
 
-  view({ attrs, children }) {
-    // all the attributes that are not the title
-    const kwargs = Object.assign({}, attrs);
-    delete kwargs.title;
+  static view({ attrs: { expanded, separated = false, title = '', ...attrs }, children }) {
+    const toolbar = m(Toolbar, { compact: true }, m(ToolbarTitle, { text: title }));
 
-    const toolbar = m(Toolbar, {
-      compact: true,
-      events: { onclick: () => { this.expand = !this.expand; } },
-    }, [
-      m(IconButton, {
-        icon: { svg: m.trust(this.expand ? icons.downarrow : icons.rightarrow) },
-      }),
-      m(ToolbarTitle, { text: attrs.title }),
-    ]);
-
-    const content = [{ any: { content: toolbar } }];
-    if (this.expand) {
-      content.push(...children.map(child => ({
-        any: {
+    return m(ExpansionPanel, {
+      expanded,
+      separated,
+      header: toolbar,
+      content: m('div', children.map(child => m(
+        'div', {
           style: {
             padding: '0 10px 10px 10px',
           },
-          content: child,
         },
-      })));
-    }
-
-    return m(Card, Object.assign({ content }, kwargs));
+        child,
+      ))),
+      ...attrs,
+    });
   }
 }
